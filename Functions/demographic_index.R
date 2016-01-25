97e0844ad3288d925a0270a52e174ab8d452c1ef
BobHoutkooper <houtkooper.b@gmail.com>
2016-01-25 16:34:53 +0100

    Delete Data

--- Functions/demographic_index.R
+++ Functions/demographic_index.R
@@ -2,31 +2,28 @@
 source("Functions/standardisation.R")
 library(raster)
 
-# standardise 
-urbanity_index = standardise(urbanity_raster)
-children_index = standardise(children_raster)
-income_index = standardise(income_raster)
-
 # create demographic index function
-demographic_index <- function(urbanity_index, urbanity_weight, children_index, children_weight, low_income_index, low_income_index_weight){
-	#' Create raster with values between 1-5 using a MCA for demographic features
+demographic_index <- function(urbanity_raster, urbanity_weight, children_raster, children_weight, low_income_raster, low_income_weight){
+	#' Create demographic index raster with a Multi-Criteria Analyses
   #'      Arg:
-  #'          in_raster (raster): extent of research area
-  #'          urbanity (int): urbanity factor, based on OAD --> CBS data
+  #'          urbanity_raster (raster): OAD values of Gelderland --> CBS data
   #'          urbanity_weight (int): population density weight
-  #'          children_index(int): index of the number of children in the area (0 -15 years)
+  #'          children_raster(raster): index of the number of children in the area (0 -15 years)
   #'          children_weight(int): weight of the children index
-  #'          income_index(int)
-  #'          
+  #'          low_income_raster(raster)
+  #'          low_income_weight(int)
   #'      Return:
-  #'        	Raster with values representing the importance of the above mentioned demographic features
+  #'        	Result of the MCA; Raster with values representing the importance of the above mentioned demographic features
  
-	# calculate  
-	Raster_urbanity_index <- overlay(urbanity_index, urbanity_weight, fun=function(x,y){x*y})
-	Raster_children_index <- overlay(children_index, children_weight, fun=function(x,y){x*y})
-	Raster_income_index <- overlay(low_income_index, low_income_weight, fun=function(x,y){x*y})
-  Raster_index_brick <- stack(Raster_urbanity_index, Raster_children_index, Raster_income_index)
+	# standardise raster values
+	urbanity_index = standardise(urbanity_raster)
+	children_index = standardise(children_raster)
+	income_index = standardise(low_income_raster)
 	
-	return(Raster_index_stack)
+	# calculate  the values for the demographic features
+	Raster_demographic_features <- overlay(urbanity_index, urbanity_weight, children_index, children_weight, low_income_index, low_income_weight, fun=function(a,b,c,d,e,f){(a*b)+(c*d)+(e*f)})
+	
+	# return MCA based raster
+	return(Raster_demographic_features)
 }
 
