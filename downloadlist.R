4078b0aa38ccf4d204f4739703788a5c3a91a846
user <m.vregelaar@gmail.com>
2016-01-26 14:54:59 +0100

    functions that downloads and pre-proces the data

---
+++ downloadlist.R
@@ -0,0 +1,8 @@
+# funstion to dowload a list of URLs
+downloadlist <- function(myURL) {
+	#' function that downloads files from a URL
+	#' Arg: myURL(str): URL of where files are dowloaded from
+	#'
+	download.file(url = myURL[[2]], destfile = paste0('downloads/',myURL[[1]],'.zip'), method = 'wget')
+	
+}
