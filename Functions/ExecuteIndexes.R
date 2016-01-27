66e4b61fdf88010dbfe54542a9ca2c7ab12438ee
BobHoutkooper <houtkooper.b@gmail.com>
2016-01-27 16:28:05 +0100

    Finished main.R, added the function 'ExecuteIndexes.R'

--- Functions/ExecuteIndexes.R
+++ Functions/ExecuteIndexes.R
@@ -4,41 +4,22 @@ ExecuteIndexes <- function(){
 	source('Functions/facilities_index.R')
 	source('Functions/inconvenience_index.R')
 	source('Functions/PublicTrans_Index.R')
+	source('Functions/WaterIndex.R')
 	source('Functions/NDVI_Index.R')
-	source('Functions/ReadInputUser.R')
-	
-	# Set variables
-	print("Would you mind living closer than 100 meter from a traintrack?")
-	railway_weight <- readinteger_weight()
-	
-	print("Would you mind living closer than 100 meter from a highway?")
-	roads_weight <- readinteger_weight()
-	
-	print("Do you want to live in a rural area?")
-	urban_boolean <-readinteger_bool()
-	
-	print("Is this important to you?")
-	urbanity_weight <- readinteger_weight()
-	
-	print("You want to live among children?")
-	children_boolean <- readinteger_bool()
-	
-	print("Is this important to you?")
-	children_weight <- readinteger_weight()
-	
-	print("You want to live among elderly people?")
-	elderly_boolean <- readinteger_bool()
-	
-	print("Is this important to you?")
-	elderly_weight <- readinteger_weight()
-	
+
 	#Execute indexes
-	PublicTrans_Index()
-	inconvenience_index(railway_weight, roads_weight)
-	facilities_index()
-	demographic_index(urban_boolean, urbanity_weight, children_boolean, children_weight, elderly_boolean, elderly_weight)
-	NDVI_Index()
-	Water_Index()
+	publicTrans <- PublicTrans_Index()
+	inconvenience <- inconvenience_index(railway_weight, roads_weight)
+	facilities <- facilities_index()
+	demographic <- demographic_index(urban_boolean, urbanity_weight, children_boolean, children_weight, elderly_boolean, elderly_weight)
+	NDVI <- NDVI_Index()
+	Water <-Water_Index()
+	
+	Stack_output <- "Output/TotalIndex.grd"
+	Total_Indexes <- stack(publicTrans, inconvenience, facilities, demographic, NDVI, Water)
+	Names(Total_Indexes) <- c("Public Transport Index", "Inconvenience Index", "Facilities Index", "Demographic Index", "NDVI Index", "Water Index")
+	writeRaster(x=Total_Indexes, filename=Stack_output)
+	return(Total_Indexes)
 }
 
 
--- main.R
+++ main.R
@@ -10,6 +10,7 @@ library(GISTools)
 library(maptools)
 library(rasterVis)
 library(sp)
+library(leaflet)
 
 # source functions
 
@@ -17,7 +18,7 @@ source('Functions/ExecuteIndexes.R')
 
 # Create directories
 data_dir = 'Data'
-output_dir = 'Ouput'
+output_dir = 'Output'
 functions_dir = 'Functions'
 download_dir = 'Downloads'
 rawdata_dir = 'Rawdata'
@@ -28,11 +29,34 @@ for (i in dir_list)
 	} 
 }
 
+# Set weights
+railway_weight <- 1
+roads_weight <- 1 
+urban_boolean <- TRUE
+urbanity_weight <-1 
+children_boolean <-TRUE
+children_weight <-1 
+elderly_boolean <-TRUE
+elderly_weight <-1 
+
 # Calculate indexes
-ExecuteIndexes()
+substack <- ExecuteIndexes()
 
 # Weights of indexes
+PT_weight <- 1
+inconvenience_weight <- 1
+demographic_weight <- 1
+facilities_weight <- 1
+water_weight <- 1
+NDVI_weight <- 1
 
-
+# weight x index
+PT_result <- PT_weight * substack[[1]]
+inconvenience_result <- inconvenience_weight * substack[[2]]
+facilities_result <- facilities_weight * substack[[3]]
+demographic_result <- demographic_weight * substack[[4]]
+NDVI_result <- NDVI_weight * substack[[5]]
+water_result <- water_weight * substack[[6]]
+	
 # Create raster
-
+End_score<- overlay(PT_result, inconvenience_result, facilities_result, demographic_result, NDVI_result, water_result, fun=function(a,b,c,d,e,f){a+b+c+d+e+f})
