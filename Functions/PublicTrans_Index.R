8ecf2168ae7fca62a44241110fc649e0379a068f
JosGoris <jos.goris91@gmail.com>
2016-01-27 15:24:11 +0100

    nog meer comments

--- Functions/PublicTrans_Index.R
+++ Functions/PublicTrans_Index.R
@@ -7,9 +7,11 @@ PublicTrans_Index <- function(){
 	source("Functions/busstations.R")
 	source("Functions/standardisation.R")
 	
+	# Get the bus and train stations
 	bus <- Bus_index()
 	train <- TrainStat_index()
 	
+	# Combine the two rasters and standardise 
 	PublicTrans <- overlay(train,bus, fun=function(a,b){a+b})
 	PublicTransScore <- standardise(PublicTrans)
 	return(PublicTransScore)
